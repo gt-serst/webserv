9183cddaa685afd1841e70ec1114d5b7283c1130
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2024-05-06 20:58:44 +0200

    Late save

--- confParser.cpp
+++ confParser.cpp
@@ -1,11 +1,17 @@
 #include "confParser.hpp"
 
+t_server_scope *parseServer(int *i, std::string buffer, t_server_scope *serverConfig)
+{
+	
+}
+
 t_server_scope *confParser(std::string buffer)
 {
-	//t_server_scope	*serverConfig = NULL;
+	t_server_scope	*serverConfig = NULL;
 	for (int i = 0; buffer[i]; i++)
 	{
-		std::cout << buffer[i];
+		if (!(serverConfig = parseServer(&i, buffer, serverConfig)))
+			return (NULL);
 	}
-	return (NULL);
+	return (serverConfig);
 }
