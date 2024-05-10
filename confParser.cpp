3bab17cbfdc2b11fee3fe713a88c869729822a41
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2024-05-10 17:42:14 +0200

    all modules done but the location one

--- confParser.cpp
+++ confParser.cpp
@@ -16,19 +16,126 @@ static bool		isServer(int i, std::string buffer)
 	return (true);
 }
 
-static t_server_scope	*isServerName(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+/*static t_server_scope	*isServerName(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+  {
+  if (buffer.substr(*i, 12) == "server_name\t")
+  {
+  int count = 0;
+ *i += 12;
+ while (buffer[*i] && buffer[*i] != '\n')
+ {
+ int	j = *i;
+ while (buffer[j] && isprint(buffer[j]) && buffer[j] != ' ')
+ j++;
+ if (j != *i && buffer[j] && (buffer[j] == '\n' || buffer[j] == ' '))
+ {
+ count++;
+ std::string *temp = NULL;
+ temp = new std::string[count];
+ if (count > 1)
+ {
+ for (int k = 0; k < count; k++)
+ temp[k] = serverConfig[*servers].server_name[k];
+ delete[] serverConfig[*servers].server_name;
+ }
+ temp[count - 1] = buffer.substr(*i, j - *i);
+ serverConfig[*servers].server_name = temp;
+ *i = j + 1;
+ std::cout << "server_name : " << serverConfig[*servers].server_name[count - 1] << std::endl;
+//return (serverConfig);
+}
+else
 {
-	(void)servers;
-	std::cout << "I : " << *i << std::endl;
-	std::cout << "buffer[*i] = " << buffer[*i] << std::endl;
+freeConfig(serverConfig, *servers);
+return (NULL);
+}
+}
+return (serverConfig);
+}
+freeConfig(serverConfig, *servers);
+return (NULL);
+}*/
+
+static t_server_scope *isServerName(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+{
+	if (buffer.substr(*i, 13) != "server_name\t\t")
+	{
+		freeConfig(serverConfig, *servers);
+		return (NULL);
+	}
+	*i += 12;
+	std::vector<std::string> names;
+	while (buffer[*i] && buffer[*i] != '\n')
+	{
+		*i += 1;
+		int j = *i;
+		while (buffer[j] && isprint(buffer[j]) && buffer[j] != ' ')
+			j++;
+		if (j != *i && buffer[j] && (buffer[j] == '\n' || buffer[j] == ' '))
+		{
+			names.push_back(buffer.substr(*i, j - *i));
+			*i = j;
+		}
+		else
+		{
+			freeConfig(serverConfig, *servers);
+			return (NULL);
+		}
+	}
+	*i += 1;
+	serverConfig[*servers].server_name = names;
+	return (serverConfig);
+}
+
+static t_server_scope *isServerErrPage(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+{
+	if (buffer.substr(*i, 12) != "error_page\t\t")
+	{
+		freeConfig(serverConfig, *servers);
+		return (NULL);
+	}
+	*i += 11;
+	std::map<int, std::string> result;
+	while (buffer[*i] && buffer[*i] != '\n')
+	{
+		*i += 1;
+		int j = *i;
+		while (buffer[j] && isdigit(buffer[j]))
+			j++;
+		int error_code = stoi(buffer.substr(*i, j - *i));
+		if (j != *i && buffer[j] && buffer[j] == ' ' && error_code >= 400 && error_code < 600)
+		{
+			j++;
+			*i = j;
+			while (buffer[j] && isprint(buffer[j]) && buffer[j] != ' ')
+				j++;
+			if (buffer[j] && (buffer[j] == ' ' || buffer[j] == '\n'))
+			{
+				result[error_code] = buffer.substr(*i, j - *i);
+				*i = j;
+			}
+			else
+			{
+				freeConfig(serverConfig, *servers);
+				return (NULL);
+			}
+		}
+		else
+		{
+			freeConfig(serverConfig, *servers);
+			return (NULL);
+		}
+	}
+	*i += 1;
+	serverConfig[*servers].error_page_paths = result;
 	return (serverConfig);
 }
 
 static t_server_scope	*isServerHost(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
 {
-	if (buffer.substr(*i, 6) == "listen" && buffer[*i + 6] == '\t' && buffer[*i + 7] == '\t')
+	if (buffer.substr(*i, 9) == "listen\t\t\t")
 	{
-		*i += 8;
+		*i += 9;
 		int j =	*i;
 		while (isdigit(buffer[j]))
 			j++;
@@ -39,48 +146,87 @@ static t_server_scope	*isServerHost(int *i, std::string buffer, t_server_scope *
 		}
 		int res = stoi(buffer.substr(*i, j - *i));
 		*i = j;
-		if (buffer[*i] != '\n' || res <= 0 || res >= 65353)
+		if (buffer[*i] != '\n' || res < 0 || res > 65353)
 		{
 			freeConfig(serverConfig, *servers);
 			return (NULL);
 		}
 		serverConfig[*servers].port = res;
+		*i += 1;
 		return (serverConfig);
 	}
 	freeConfig(serverConfig, *servers);
 	return (NULL);
 }
 
-static t_server_scope	*isServerErrPage(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+static t_server_scope *isServerUploadPath(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
 {
-	(void)servers;
-	std::cout << "I : " << *i << std::endl;
-	std::cout << "buffer[*i] = " << buffer[*i] << std::endl;
-	return (serverConfig);
+	if (buffer.substr(*i, 13) != "upload_path\t\t")
+	{
+		freeConfig(serverConfig, *servers);
+		return (NULL);
+	}
+	*i += 13;
+	int j = *i;
+	while (buffer[j] && buffer[j] != '\n' && buffer[j] != ' ' && isprint(buffer[j]))
+		j++;
+	if (j != *i && buffer[j] && buffer[j] == '\n')
+	{
+		serverConfig[*servers].upload_path = buffer.substr(*i, j - *i);
+		*i = j + 1;
+		return (serverConfig);
+	}
+	freeConfig(serverConfig, *servers);
+	return (NULL);
+}
+
+static t_server_scope *isServerCGI(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
+{
+	if (buffer.substr(*i, 6) != "CGI\t\t\t")
+	{
+		freeConfig(serverConfig, *servers);
+		return (NULL);
+	}
+	*i += 6;
+	int j = *i;
+	while (buffer[j] && buffer[j] != '\n' && buffer[j] != ' ' && isprint(buffer[j]))
+		j++;
+	if (j != *i && buffer[j] && buffer[j] == '\n')
+	{
+		serverConfig[*servers].cgi_path = buffer.substr(*i, j - *i);
+		*i = j + 1;
+		return (serverConfig);
+	}
+	freeConfig(serverConfig, *servers);
+	return (NULL);
 }
 
 static t_server_scope	*isServerMaxBodySize(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
 {
-	(void)servers;
-	std::cout << "I : " << *i << std::endl;
-	std::cout << "buffer[*i] = " << buffer[*i] << std::endl;
-	return (serverConfig);
-}
-
-static t_server_scope	*isServerCGI(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
-{
-	(void)servers;
-	std::cout << "I : " << *i << std::endl;
-	std::cout << "buffer[*i] = " << buffer[*i] << std::endl;
-	return (serverConfig);
-}
-
-static t_server_scope	*isServerUploadPath(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
-{
-	(void)servers;
-	std::cout << "I : " << *i << std::endl;
-	std::cout << "buffer[*i] = " << buffer[*i] << std::endl;
-	return (serverConfig);
+	if (buffer.substr(*i, 21) == "client_max_body_size\t")
+	{
+		*i += 21;
+		int j =	*i;
+		while (isdigit(buffer[j]))
+			j++;
+		if (j == *i)
+		{
+			freeConfig(serverConfig, *servers);
+			return (NULL);
+		}
+		int res = stoi(buffer.substr(*i, j - *i));
+		*i = j;
+		if (buffer[*i] != '\n' || res < 1 || res > 1000000)
+		{
+			freeConfig(serverConfig, *servers);
+			return (NULL);
+		}
+		serverConfig[*servers].max_body_size = res;
+		*i += 1;
+		return (serverConfig);
+	}
+	freeConfig(serverConfig, *servers);
+	return (NULL);
 }
 
 static t_server_scope	*isServerLocation(int *i, std::string buffer, t_server_scope *serverConfig, int *servers)
@@ -96,12 +242,18 @@ static t_server_scope	*getServerConfig(int *i, std::string buffer, t_server_scop
 	//While inside a 'server' block, calling the appropriate function according to the first character.
 	while (buffer[*i] && buffer[*i] != '}')
 	{
-		if (buffer[*i] != '\t')
+		if (buffer[*i] && buffer[*i] != '\t' && buffer[*i] != '\n')
 		{
 			freeConfig(serverConfig, *servers);
 			return (NULL);
 		}
+		if (buffer[*i] && buffer[*i] == '\n')
+		{
+			*i += 1;
+			continue ;
+		}
 		*i += 1;
+		std::cout << buffer[*i] << std::endl;
 		switch (buffer[*i])
 		{
 			case 's' :
@@ -152,6 +304,9 @@ static t_server_scope	*getServerConfig(int *i, std::string buffer, t_server_scop
 					freeConfig(serverConfig, *servers);
 					return (NULL);
 				}
+				break ;
+			case '\n' :
+				*i += 1;
 				break ;
 			default :
 				freeConfig(serverConfig, *servers);
--- confParser.hpp
+++ confParser.hpp
@@ -5,6 +5,9 @@
 # include <iostream>
 # include <fstream>
 # include <cctype>
+# include <vector>
+# include <map>
+
 //----------ENUMS----------//
 
 typedef enum e_allowed_methods
@@ -18,29 +21,29 @@ typedef enum e_allowed_methods
 
 typedef struct s_locations
 {
-	std::string	location_path; //route path (on the website)
-	std::string	root_path; //path where files should be searched from
-	std::string	**redirections;
-	std::string	default_path; //default path if request is a directory
-	std::string	*allowed_methods;
-	bool		auto_index; //is directory listing allowed?
+	std::string				location_path; //route path (on the website)
+	std::string				root_path; //path where files should be searched from
+	std::map<std::string, std::string>	redirections;
+	std::vector<std::string>		default_path; //default path if request is a directory
+	std::vector<std::string>		allowed_methods;
+	bool					auto_index; //is directory listing allowed?
 }	t_locations;
 
 typedef struct s_server_scope
 {
-	int		port;
-	int		max_body_size;
-	std::string	server_name;
-	std::string	cgi_path;
-	std::string	upload_path;
-	std::string	**error_page_paths;
-	t_locations	*locations;
+	int					port;
+	int					max_body_size;
+	std::vector<std::string>		server_name;
+	std::map<int, std::string>		error_page_paths;
+	std::map<std::string, t_locations>	locations;
+	std::string				cgi_path;
+	std::string				upload_path;
 }	t_server_scope;
 
 //----------FUNCTIONS----------//
 
-int		webserv(int argc, char **argv);
-std::string	confChecker(int argc, char **argv);
-t_server_scope	*confParser(std::string buffer);
+int						webserv(int argc, char **argv);
+std::string					confChecker(int argc, char **argv);
+t_server_scope					*confParser(std::string buffer);
 
 #endif
