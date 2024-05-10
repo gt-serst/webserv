885bad81b25c5b2b63517fd7168d2fc27f460866
Geraud t'Serstevens <geraudts@gmail.com>
2024-05-10 14:59:46 +0200

    multiple servers

--- examples/my_little_http_server.cpp
+++ examples/my_little_http_server.cpp
@@ -15,8 +15,10 @@ int	main(int argc, char ** argv)
 	int		rc;
 	int		flags;
 
-	struct pollfd	pollfds[MAX_FD];
-	memset(pollfds, 0, sizeof(pollfds));
+	struct pollfd	pollfd_clients[MAX_FD];
+	struct pollfd	pollfd_servers[MAX_FD];
+	memset(pollfd_clients, 0, sizeof(pollfd_clients));
+	memset(pollfd_servers, 0, sizeof(pollfd_servers));
 
 	for (int x = 0; x < 2; x++)
 	{
@@ -44,47 +46,151 @@ int	main(int argc, char ** argv)
 		if (rc < 0)
 			perror("Bind() failed");
 
-		int	connection_backlog = 5;
+		int	connection_backlog = 50;
 		rc = listen(server_fd[x], connection_backlog);
 		if (rc < 0)
 			perror("Listen() failed");
 
-		pollfds[x].fd = server_fd[x];
-		pollfds[x].events = POLLIN;
+		pollfd_servers[x].fd = server_fd[x];
+		pollfd_servers[x].events = POLLIN;
 	}
 
 	struct sockaddr_in client_addr;
 	int client_addr_len = sizeof(client_addr);
 
-	std::cout << server_fd[0] << std::endl;
-	std::cout << server_fd[1] << std::endl;
-
+	/*rc = poll(pollfd_servers, MAX_FD, -1);
+	if (rc < 0)
+		perror("Poll() failed");*/
+	bool pollused = false;
+	bool writing = false;
 	while (1)
 	{
-		rc = poll(pollfds, MAX_FD, -1);
-		if (rc < 0)
-			perror("Poll() failed");
-
+		std::cout << "New loop" << std::endl;
+		rc = poll(pollfd_servers, MAX_FD, -1);
 		for (int x = 0; x < 2; x++)
+		{
+			std::cout << "Listening loop" << std::endl;
+			/*if (start == true)
+			{
+				std::cout << "Out-loop Poll Initialization" << std::endl;
+				rc = poll(pollfd_clients, MAX_FD, -1);
+			}*/
+			if (pollfd_servers[x].revents & POLLIN)
+			{
+				std::cout << "Waiting for connection" << std::endl;
+				
+				client_fd = accept(pollfd_servers[x].fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+				if (client_fd < 0)
+					perror("Accept() failed");
+				
+				std::cout << "Connected" << std::endl;
+
+				flags = fcntl(client_fd, F_GETFL, 0);
+				if (flags < 0)
+					perror("Fcntl() failed");
+				fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
+
+				for (int z = 0; z < MAX_FD; z++) 
+				{
+					std::cout << "Poll elem initialization loop" << std::endl;
+					std::cout << pollfd_clients[z].fd << std::endl;
+					if (pollfd_clients[z].fd == 0 && z == 0)
+					{
+						std::cout << "First elem initialization" << std::endl;
+						pollfd_clients[z].fd = client_fd;
+						pollfd_clients[z].events = POLLIN;
+						std::cout << "In-loop Poll initialization" << std::endl;
+						rc = poll(pollfd_clients, MAX_FD, -1);
+						if (rc < 0)
+							perror("Poll() failed");
+						pollused = true;
+						writing = false;
+						break;
+					}
+					else if (pollfd_clients[z].fd == 0)
+					{
+						std::cout << "Elem initialization" << std::endl;
+						pollfd_clients[z].fd = client_fd;
+						pollfd_clients[z].events = POLLIN;
+						pollused = true;
+						writing = false;
+						break;
+					}
+				}
+				for (int y = 0; y < MAX_FD; y++)
+				{
+					std::cout << "Writing loop" << std::endl;
+					if (pollfd_clients[y].revents & POLLIN)
+					{
+						char	buffer[255];
+						int		len;
+						std::string response;
+
+						std::cout << "Waiting to write" << std::endl;
+						rc = recv(pollfd_clients[y].fd, buffer, sizeof(buffer), 0);
+						if (rc < 0)
+							perror("Recv() failed");
+						else
+						{
+							std::cout << buffer << std::endl;
+
+							std::string response = "HTTP/1.1 200 OK\r\n";
+							std::string body = "<!DOCTYPE html>\n"
+                			"<html>\n"
+                 			"<head>\n"
+                 			"<title>Welcome to nginx!</title>\n"
+                 			"<style>\n"
+                 			"html { color-scheme: light dark; }\n"
+                 			"body { width: 35em; margin: 0 auto;\n"
+                 			"font-family: Tahoma, Verdana, Arial, sans-serif; }\n"
+                 			"</style>\n"
+                 			"</head>\n"
+                 			"<body>\n"
+                 			"<h1>Welcome to nginx!</h1>\n"
+                 			"<p>If you see this page, the nginx web server is successfully installed and\n"
+                 			"working. Further configuration is required.</p>\n"
+                 			"<p>For online documentation and support please refer to\n"
+                 			"<a href=\"http://nginx.org/\">nginx.org</a>.<br/>\n"
+                 			"Commercial support is available at\n"
+                 			"<a href=\"http://nginx.com/\">nginx.com</a>.</p>\n"
+                 			"<p><em>Thank you for using nginx.</em></p>\n"
+                 			"</body>\n"
+                 			"</html>\n";
+
+							response = std::string("HTTP/1.1 200 OK") + std::string("Content-Type: application/octet-stream\r\nContent-Length: ") + std::to_string(body.length()) + std::string("\r\n\r\n") + body + std::string("\r\n\r\n");
+
+							len = response.length();
+							rc = send(pollfd_clients[y].fd, response.c_str(), len, 0);
+							if (rc < 0)
+								perror("Send() failed");
+						}
+						close(pollfd_clients[y].fd);
+						writing = true;
+					}
+				}
+			}
+			if (pollused == true && writing == true)
+				rc = poll(pollfd_clients, MAX_FD, -1);
+			pollused = false;
+		}
+		//rc = poll(pollfd_clients, MAX_FD, -1);
+		std::cout << "End loop" << std::endl;
+
+		/*for (int x = 0; x < 2; x++)
 		{
 			for (int y = 0; y < MAX_FD; y++)
 			{
 
-				/** Condition pour s'assurer que l'event qui a eu lieu survient bien sur le serveur ou la boucle se situe actuellement  **/
-				if (!(pollfds[y].revents & POLLIN))
+				 //Condition pour s'assurer que l'event qui a eu lieu survient bien sur le serveur ou la boucle se situe actuellement
+				if (!(pollfd_servers[x].revents & POLLIN) && !(pollfd_clients[y].revents & POLLIN))
 					continue;
-				if (pollfds[y].fd == server_fd[x])
+				if ((pollfd_servers[x].revents & POLLIN) && !(pollfd_clients[y].revents & POLLIN))
 				{
 					std::cout << "New connection" << std::endl;
-					client_fd = accept(server_fd[x], (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+					client_fd = accept(pollfd_servers[x].fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
-					/** Condition qui rejette tous les serveurs qui n'ont pas recu de connexions **/	
+					// Condition qui rejette tous les serveurs qui n'ont pas recu de connexions	
 					if (client_fd < 0)
-					{
-						std::cout << "Connection rejected" << std::endl;
-						continue;
-					}
-					else if (client_fd < 0 && x == 1)
 						perror("Accept() failed");
 
 					flags = fcntl(client_fd, F_GETFL, 0);
@@ -92,18 +198,28 @@ int	main(int argc, char ** argv)
 						perror("Fcntl() failed");
 					fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
 
-					for (int z = 1; z < MAX_FD; z++) 
-					{	
-						if (pollfds[z].fd == 0)
+					for (int z = 0; z < MAX_FD; z++) 
+					{
+						if (pollfd_clients[z].fd == 0 && z == 0)
 						{
-							pollfds[z].fd = client_fd;
-							pollfds[z].events = POLLIN;
+							std::cout << "z == 0" << std::endl;
+							pollfd_clients[z].fd = client_fd;
+							pollfd_clients[z].events = POLLIN;
+							rc = poll(pollfd_clients, MAX_FD, -1);
+							if (rc < 0)
+								perror("Poll() failed");
+							break;
+						}
+						else if (pollfd_clients[z].fd == 0)
+						{
+							pollfd_clients[z].fd = client_fd;
+							pollfd_clients[z].events = POLLIN;
 							break;
 						}
 					}
 				}
 
-				/** Condition pour s'assurer qu'on se trouve sur la bonne socket a lire et pas sur la socket du serveur qui na pas recu de connexions **/
+				// Condition pour s'assurer qu'on se trouve sur la bonne socket a lire et pas sur la socket du serveur qui na pas recu de connexions
 				else
 				{
 					std::cout << "Right socket" << std::endl;
@@ -111,7 +227,9 @@ int	main(int argc, char ** argv)
 					int		len;
 					std::string response;
 
-					rc = recv(pollfds[y].fd, buffer, sizeof(buffer), 0);
+					std::cout << pollfd_clients[y].fd << std::endl;
+					std::cout << pollfd_clients[y].revents << std::endl;
+					rc = recv(pollfd_clients[y].fd, buffer, sizeof(buffer), 0);
 					if (rc < 0)
 						perror("Recv() failed");
 					else
@@ -144,18 +262,18 @@ int	main(int argc, char ** argv)
 						response = std::string("HTTP/1.1 200 OK") + std::string("Content-Type: application/octet-stream\r\nContent-Length: ") + std::to_string(body.length()) + std::string("\r\n\r\n") + body + std::string("\r\n\r\n");
 
 						len = response.length();
-						rc = send(pollfds[y].fd, response.c_str(), len, 0);
+						rc = send(pollfd_clients[y].fd, response.c_str(), len, 0);
 						if (rc < 0)
 							perror("Send() failed");
 
 						std::cout << "Message sent" << std::endl;
 					}
 				}
-				close(pollfds[y].fd);
+				//close(pollfd_clients[y].fd);
 			}
-		}
+		}*/
 	}
 	for (int x = 0; x < 2; x++)
-		close(server_fd[x]);
+		close(pollfd_servers[x].fd);
 	return (0);
 }
