0739f4637e9d62d6317d3a377b1963b00b6cf891
Geraud t'Serstevens <geraudts@gmail.com>
2024-05-23 17:44:36 +0200

    beginning response generation

--- srcs/response/Response.hpp
+++ srcs/response/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: gt-serst <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:12:17 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/05/23 17:12:31 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:41:43 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,31 @@
 class Response{
 
 	public:
-		void	handleDirective(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
+		void		handleDirective(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
 
 	private:
 		Response();
 		~Response();
-		bool	isLocationRooted(std::string& path, std::string root, t_locations loc);
-		void	getFileType(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
-		void	isDir(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
-		void	isFile(std::string path, t_locations loc, Request *req);
-		bool	isMethodAllowed(t_locations loc, Request *req);
-		void	runDirMethod(std::string path, t_locations loc, Request *req);
-		void	isAutoIndex(DIR *dr, std::string path, t_locations loc);
-		void	deleteDir(DIR *dr, std::string path);
-		void	isCGI(std::string path, t_locations loc, Request *req);
-		void	runFileMethod(std::string path, t_locations loc, Request *req);
-		void	openFile(std::string path);
-		void	uploadFile(std::string path, Request *req);	
-		void	deleteFile(std::string path);
+		bool		isLocationRooted(std::string& path, std::string root, t_locations loc);
+		void		getFileType(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
+		void		isDir(std::string path, t_locations loc, std::map<std::string, t_locations> routes, Request *req);
+		void		isFile(std::string path, t_locations loc, Request *req);
+		bool		isMethodAllowed(t_locations loc, Request *req);
+		void		runDirMethod(std::string path, t_locations loc, Request *req);
+		void		isAutoIndex(DIR *dr, std::string path, t_locations loc);
+		void		deleteDir(DIR *dr, std::string path);
+		void		isCGI(std::string path, t_locations loc, Request *req);
+		void		runFileMethod(std::string path, t_locations loc, Request *req);
+		void		openFile(std::string path);
+		void		uploadFile(std::string path, Request *req);	
+		void		deleteFile(std::string path);
 		std::string	_response;
+		std::string	_http_version;
+		std::string	_code_statut;
+		std::string	_statut_message;
+		std::string	_content_type;
+		std::string	_content_length;
+		std::string	_body;
 };
 
 #endif
