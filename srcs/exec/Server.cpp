d583247885422924eaa094407f104cd9a4064478
Geraud t'Serstevens <geraudts@gmail.com>
2024-06-19 15:43:16 +0200

    update

--- srcs/exec/Server.cpp
+++ srcs/exec/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 09:59:24 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/06/19 15:06:17 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/06/19 15:25:07 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -237,7 +237,7 @@ int	Server::sendResponse(int client_fd){
 	int	len;
 
 	len = _requests[client_fd].length();
-	//std::cout << _requests[client_fd] << std::endl;
+	std::cout << _requests[client_fd] << std::endl;
 	rc = send(client_fd, _requests[client_fd].c_str(), len, 0);
 	if (rc == 0 || rc == -1)
 	{
