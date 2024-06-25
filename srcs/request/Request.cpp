6941d081e08fa252f416564807e97ccd20bfe7ab
Maxime Dor <mdor@shi-r6-p2.s19.be>
2024-06-25 11:23:02 +0200

    _hostname check fix

--- srcs/request/Request.cpp
+++ srcs/request/Request.cpp
@@ -906,22 +906,6 @@ bool	Request::handle_headers()
 		_error_msg = "Hostname in the URI does not match the hostname in the Host header";
 		return true;
 	}
-	int i = 0;
-	std::vector<std::string> tmp = _server.getConfig().server_name;
-	for (std::vector<std::string>::iterator it = tmp.begin(); it != tmp.end(); ++it)
-	{
-		if (line.compare(*it + "\r") == 0)
-			break;
-		i++;
-	}
-	if (tmp.begin() + i == tmp.end())
-	{
-		tmp.clear();
-		_error_code = 400;
-		_error_msg = "Hostname in the request does not match the hostname in the server config";
-		return true;
-	}
-	tmp.clear();
     size_t pos = line.find(":");
     if (pos != std::string::npos)
     {
@@ -938,6 +922,22 @@ bool	Request::handle_headers()
     {
         _hostname = line;
     }
+	int i = 0;
+	std::vector<std::string> tmp = _server.getConfig().server_name;
+	for (std::vector<std::string>::iterator it = tmp.begin(); it != tmp.end(); ++it)
+	{
+		if (_hostname.compare(*it) == 0)
+			break;
+		i++;
+	}
+	if (tmp.begin() + i == tmp.end())
+	{
+		tmp.clear();
+		_error_code = 400;
+		_error_msg = "Hostname in the request does not match the hostname in the server config";
+		return true;
+	}
+	tmp.clear();
 	line = getHeader("Content-Length");
 	if (!line.empty())
 	{
